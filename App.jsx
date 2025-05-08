42eb29e85720a3acb09f05179b9b4be4290584e9
MohdGulfam02 <47323895+MohdGulfam02@users.noreply.github.com>
2025-05-08 06:41:36 -0400

    first commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+
+import "./App.css";
+import { Outlet } from "react-router";
+
+export default function App() {
+  const [isDark, setIsDark] = useState(JSON.parse(localStorage.getItem('isDarkMode')));
+
+  return (
+    <>
+      <Header theme={[isDark, setIsDark]}/>
+      <Outlet context={[isDark, setIsDark]}/>
+    </>
+  );
+}
